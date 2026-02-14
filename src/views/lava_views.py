409f8754608c6a8c48e2cb3e5e7097dc8f353de9
indra1080 <momo7859072@email.com>
2026-02-14 19:28:24 +0100

    lave et tresors

--- src/views/lava_views.py
+++ src/views/lava_views.py
@@ -1,8 +1,8 @@
 import pygame
 from views.tile_view import TileView
 from tiles.colors import LAVA
-
+image=pygame.image.load("src/views/tile_3_19.png")
 class LavaTileView(TileView):
     """Cette classe est responsable de l'affichage des tiles d'eau dans le monde de jeu."""
     def draw(self, screen: pygame.Surface, rect: tuple[int, int, int, int]):
-        pygame.draw.rect(screen, LAVA, rect)
+        screen.blit(pygame.transform.scale(image,(40,40)),rect)
--- src/views/tresor_view.py
+++ src/views/tresor_view.py
@@ -1,8 +1,8 @@
 import pygame
 from views.tile_view import TileView
 from tiles.colors import BLACK
-
+image=pygame.image.load("src/views/tile_37_9.png")
 class TresorTileView(TileView):
     """Cette classe est responsable de l'affichage des tiles tresor dans le monde de jeu."""
     def draw(self, screen: pygame.Surface, rect: tuple[int, int, int, int]):
-        pygame.draw.rect(screen, BLACK, rect)
+        screen.blit(pygame.transform.scale(image,(40,40)),rect)
